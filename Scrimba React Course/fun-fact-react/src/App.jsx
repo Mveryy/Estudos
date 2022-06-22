1b3c0ed1628fab262efda85f0b0102acb8cd59ad
Mvery <matheus.verissimo@outlook.com>
2022-06-22 15:28:00 -0300

    color switch

--- Scrimba React Course/fun-fact-react/src/App.jsx
+++ Scrimba React Course/fun-fact-react/src/App.jsx
@@ -1,11 +1,16 @@
 import Nav from "./components/Nav";
 import MainContent from "./components/MainContent";
+import { useState } from "react";
 
 export default function App() {
-    return (
-        <>
-            <Nav />
-            <MainContent />
-        </>
-    );
+  const [darkMode, setDarkMode] = useState(false);
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+  return (
+    <>
+      <Nav darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <MainContent darkMode={darkMode} />
+    </>
+  );
 }
--- Scrimba React Course/fun-fact-react/src/components/Nav/index.jsx
+++ Scrimba React Course/fun-fact-react/src/components/Nav/index.jsx
@@ -1,11 +1,17 @@
 import "./index.css";
 
-export default function Nav() {
-    return (
-        <nav className="nav">
-            <img className="nav--logo" src="./src/images/react-logo.png" />
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
-        </nav>
-    );
+export default function Nav({ darkMode, toggleDarkMode }) {
+  return (
+    <nav className={darkMode ? "nav dark" : "nav light"}>
+      <img className="nav--logo" src="./src/images/react-logo.png" />
+      <h3 className={darkMode ? "" : "h3-light"}>ReactFacts</h3>
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
+    </nav>
+  );
 }
